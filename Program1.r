b2268b4d46af90f5a82bb22c5357d8bfd97041fd
Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
2021-12-11 22:33:28 +0100

    Added EC series on the quadratic error measured daily

--- Program1.r
+++ Program1.r
@@ -21,6 +21,9 @@ for (year in sort(unique(n118$Year))) {
 #       " & ", strsplit(sum[6], ":")[[1]][2], "\\\\ \n")
 # }
 
+n118.CGrade.Year.plot <- ggplot(n118.Grades.Year.melt, aes(x=Grade, y=Count, group=Year, colour=Year))
+n118.Grades.Year.plot+geom_line()
+
 # Tests de independencia sobre el año
 n118.CGrade.Year.lm <- lm(Grade~Year, n118.CGrade.Year)
 n118.CGrade.Year.aov <- aov( n118.CGrade.Year.lm)
@@ -87,4 +90,72 @@ t.test(Grade~Leader,n118.NO.CGrade.LeaderAD)
 # D-A  0.02367742 -0.6579991 0.70535390 0.9962488
 # L-A -0.52107730 -1.0944751 0.05232049 0.0829032
 # L-D -0.54475472 -1.1600371 0.07052771 0.0937293
-
+FullScrumADL <- read.csv("FullScrumADL.csv", row.names=1)
+FullScrumADL.lm<-lm(EC~Leader,FullScrumADL)
+FullScrumADL.aov <- aov(FullScrumADL.lm)
+summary(FullScrumADL.aov)
+# Df Sum Sq Mean Sq F value Pr(>F)
+# Leader         2   4.89  2.4437   107.5 <2e-16 ***
+#   Residuals   6133 139.36  0.0227
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+FullScrumADL.tukey <- TukeyHSD(FullScrumADL.aov)
+FullScrumADL.tukey
+# Tukey multiple comparisons of means
+# 95% family-wise confidence level
+#
+# Fit: aov(formula = FullScrumADL.lm)
+#
+# $Leader
+# diff          lwr         upr     p adj
+# D-A  0.07278430  0.060193214  0.08537538 0.0000000
+# L-A  0.01181177  0.001092603  0.02253094 0.0265295
+# L-D -0.06097253 -0.072281607 -0.04966345 0.0000000
+plot(FullScrumADL.tukey)
+FullScrumADL.boxplot <- boxplot(EC~Leader,FullScrumADL)
+FullScrumADL.boxplot$out
+# [1]  0.479  0.499  0.519  0.538  0.515  0.534  0.554  0.573  0.528  0.508  0.528 -0.315 -0.295  0.482 -0.490 -0.470 -0.472
+# [18] -0.463 -0.444 -0.424 -0.415 -0.396 -0.376 -0.356 -0.337 -0.317 -0.297  0.472  0.477  0.478  0.469  0.488  0.479  0.499
+# [35]  0.605  0.624  0.626  0.646  0.665  0.602  0.621  0.593  0.613  0.632  0.591  0.611  0.612  0.632  0.590  0.500  0.519
+# [52]  0.539  0.534  0.553  0.573  0.593  0.550  0.570  0.590  0.572  0.592  0.611  0.613  0.632  0.652  0.671  0.691  0.485
+# [69]  0.505  0.525  0.492  0.511  0.484  0.504  0.524  0.491 -0.291 -0.395 -0.375 -0.355 -0.336 -0.316 -0.297  0.500  0.519
+# [86]  0.539  0.505  0.525  0.545  0.564  0.573  0.593  0.487  0.507  0.526  0.546  0.565  0.585  0.488  0.508  0.528  0.547
+# [103]  0.567  0.495  0.514  0.534
+FullScrumADL.NO <- FullScrumADL[!(FullScrumADL$EC %in% FullScrumADL.boxplot$out),]
+summary(FullScrumADL)
+# Leader                EC
+# Length:6136        Min.   :-0.4900
+# Class :character   1st Qu.: 0.0060
+# Mode  :character   Median : 0.0940
+# Mean   : 0.1147
+# 3rd Qu.: 0.2040
+# Max.   : 0.6910
+summary(FullScrumADL.NO)
+# Leader                EC
+# Length:6008        Min.   :-0.278
+# Class :character   1st Qu.: 0.005
+# Mode  :character   Median : 0.091
+# Mean   : 0.109
+# 3rd Qu.: 0.197
+# Max.   : 0.582
+FullScrumADL.NO.lm<-lm(EC~Leader,FullScrumADL.NO)
+FullScrumADL.NO.aov<-aov(FullScrumADL.NO.lm)
+summary(FullScrumADL.NO.aov)
+# Df Sum Sq Mean Sq F value Pr(>F)
+# Leader         2   4.04  2.0201   105.4 <2e-16 ***
+#   Residuals   6005 115.14  0.0192
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+FullScrumADL.NO.tukey <-TukeyHSD (FullScrumADL.NO.aov)
+FullScrumADL.NO.tukey
+# Tukey multiple comparisons of means
+# 95% family-wise confidence level
+#
+# Fit: aov(formula = FullScrumADL.NO.lm)
+#
+# $Leader
+# diff          lwr         upr     p adj
+# D-A  0.064020926  0.052328961  0.07571289 0.0000000
+# L-A  0.005111773 -0.004835529  0.01505908 0.4503998
+# L-D -0.058909153 -0.069413993 -0.04840431 0.0000000
+plot(FullScrumADL.NO.tukey)
